1fbc30989df271be6e91d18ddc8879b39a9d274e
Silvio Martignetti <silviomartignetti@live.com>
2021-04-22 17:10:20 +0300

    started working on models

---
+++ OnlinePrintingService/Models/dbOPScontext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace OnlinePrintingService.Models
+{
+    public class dbOPScontext:DbContext
+    {
+        public dbOPScontext() : base("OnlinePrintingService") { }
+        public DbSet<Product> Product { get; set; }
+        public DbSet<Order> Order { get; set; }
+
+
+
+    }
+}

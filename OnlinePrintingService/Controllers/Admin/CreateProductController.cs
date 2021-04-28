7cf33633f9be460f135efc81fc0e4f05be8f558c
Silvio Martignetti <silviomartignetti@live.com>
2021-04-28 20:03:13 +0300

    added products view

--- OnlinePrintingService/Controllers/Admin/CreateProductController.cs
+++ OnlinePrintingService/Controllers/Admin/CreateProductController.cs
@@ -1,9 +1,5 @@
 ﻿using OnlinePrintingService.Models;
 using OnlinePrintingService.ViewModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace OnlinePrintingService.Controllers.Admin

0eaa6ef9e7903e8a5cbdae2589851abac1957ae2
Silvio Martignetti <silviomartignetti@live.com>
2021-04-28 18:57:22 +0300

    worked on user orders

--- OnlinePrintingService/Controllers/User/UserOrderController.cs
+++ OnlinePrintingService/Controllers/User/UserOrderController.cs
@@ -1,5 +1,6 @@
 ﻿using OnlinePrintingService.Models;
 using OnlinePrintingService.ViewModel;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -10,8 +11,24 @@ namespace OnlinePrintingService.Controllers.User
         public ActionResult Index()
         {
 
-            return View();
+            using (var context = new dbOPScontext())
+            {
+                List<string> productNames = context.Product.ToList().ConvertAll(p => p.ProductName);
+                return View(productNames);
+            }
         }
+
+        public ActionResult getProductSizes(string productName)
+        {
+
+            using (var context = new dbOPScontext())
+            {
+                List<string> productSizes = (List<string>)context.Product.Where(p => p.ProductName.Equals(productName)).ToList().ConvertAll(p => p.ProductSize);
+                return View(productSizes);
+            }
+        }
+
+
 
         [HttpPost]
         public ActionResult createOrder(OrdersViewModel orderViewModel)

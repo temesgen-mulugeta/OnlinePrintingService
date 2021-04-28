95e56162731125b87e9cf130fee1fc2d0691719b
temesgen-mulugeta <temesgen_m@github.com>
2021-04-28 21:21:45 +0300

    product list

--- OnlinePrintingService/ViewModel/CreateProductViewModel.cs
+++ OnlinePrintingService/ViewModel/CreateProductViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,13 @@ namespace OnlinePrintingService.ViewModel
 {
     public class CreateProductViewModel
     {
+        [Required(ErrorMessage = "Product Name can't be blank")]
+        public string ProductName { get; set; }
+
+        [Required(ErrorMessage = "Product Size can't be blank")]
+        public string ProductSize { get; set; }
+
+        [Required(ErrorMessage = "Product Price can't be blank")]
+        public decimal Price { get; set; }
     }
 }

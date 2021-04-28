78a113a14a812044ef80a27f56bc1d5cb8ad1f36
temesgen-mulugeta <temesgen_m@github.com>
2021-04-28 18:06:30 +0300

    added orderviewmodel

--- OnlinePrintingService/Controllers/OrderController.cs
+++ OnlinePrintingService/Controllers/OrderController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using OnlinePrintingService.ViewModel;
 
 namespace OnlinePrintingService.Controllers
 {
@@ -15,6 +16,11 @@ namespace OnlinePrintingService.Controllers
             using (var context = new dbOPScontext())
             {
                 List<Order> orders = context.Order.ToList();
+                List<SelectListItem> items = new List<SelectListItem>();
+                items.Add(new SelectListItem { Text = "BC", Value = "1" });
+                items.Add(new SelectListItem { Text = "CC", Value = "2" });
+                items.Add(new SelectListItem { Text = "DC", Value = "3" });
+                ViewBag.ProductName = items;
                 return View(orders);
             }
         }
--- OnlinePrintingService/ViewModel/OrdersViewModel.cs
+++ OnlinePrintingService/ViewModel/OrdersViewModel.cs
@@ -1,11 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ComponentModel.DataAnnotations;
 using System.Web;
 
 namespace OnlinePrintingService.ViewModel
 {
+    public enum ProductName
+    {
+
+    }
     public class OrdersViewModel
     {
+        [Required(ErrorMessage = "Product Name can't be blank")]
+        public string ProductName { get; set; }
+
+        [Required(ErrorMessage = "Product Size can't be blank")]
+        public string ProductSize { get; set; }
+
+        [Required(ErrorMessage = "Product Price can't be blank")]
+        public int Quantity { get; set; }
+
+
+
+
     }
 }
